57ff993c9145aed75e7ed4e7501f2ce70eb35243
timmystroge <timmystroge75@gmail.com>
2023-05-14 14:44:57 +0100

    Setting Up Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
-import { useState } from 'react'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-   <>
-     <h1 className="text-3xl font-bold underline ">
-      Hello world!
-    </h1>
-   </>
-  )
+    <>
+      <h1 className="text-secondary font-black">THIS IS MASSIVELY!</h1>
+      <p className="text-test lol">what you want free</p>
+    </>
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      white: "#ffffff",
+      black: "#000000",
+      primary: "#212931",
+      secondary: "#1e252d",
+      grayish: "#eeeeee",
+      lightGray: "#f5f5f5",
+      text1: "#909498",
+      text2: "#717981",
+      test: "red",
+    },
+    fontFamily: {
+      bold: "bold",
+      boldItalic: "BoldItalic",
+      light: "light",
+      lightItalic: "lightItalic",
+      black: "black",
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
